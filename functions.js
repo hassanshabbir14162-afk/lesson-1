f09e659898d8e371f48df86e3cf3dc584c0dd31a
hassanshabbir14162-afk <hassanshabbir14162@gmail.com>
2026-05-13 17:49:44 +0100

    functions

---
+++ functions.js
@@ -0,0 +1,25 @@
+//functions 
+
+function add(first,second,third,fourth){
+    console.log(first+second+third+fourth)
+}
+
+add(12,67,93,1056)
+
+function subtract(beginning,middle,end){
+    console.log(beginning-middle-end)
+}
+
+subtract(263,365,0374)
+
+function divide(start,inbetween,finish){
+    console.log(start/inbetween/finish)
+}
+
+divide(8192,64,16)
+
+function multiply(a,b,c,d){
+    console.log(a*b*c*d)
+}
+
+multiply(16,32,8,2)
