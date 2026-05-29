3e96845c4a1d03fbfd68b0de3bcd5ba0c8298549
hassanshabbir14162-afk <hassanshabbir14162@gmail.com>
2026-05-29 06:12:39 +0100

    arrays

--- array.js
+++ array.js
@@ -1,2 +1,5 @@
 let a=["London","Paris","Berlin","Madrid"];
 console.log(a[2]);
+
+let a2=["apple","banana","mango","peach","pineapple"];
+a2.pop("");
---
+++ array2.js
@@ -0,0 +1,3 @@
+let a2=["apple","banana","mango","peach","pineapple"];
+   let str= a2.length;
+    console.log(a2.length)
